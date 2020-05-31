84704f93d57dea844ef5a3427c30fce4c90cd370
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2020-05-30 21:14:47 -0500

    feat: example of reader file

---
+++ cpp/using-catch2/reader/reader.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+
+class Reader
+{
+    std::vector<std::string> split(const std::string& line, const char delimiter)
+    {
+        std::vector<std::string> result;
+        return result;
+    }
+};
